09c92ae6f799ed039b67d87f7da1f7121d518cfc
CaptainPinkTurd <dominhdang2004@gmail.com>
2024-05-08 23:53:13 +0700

    sprites and environment set up

---
+++ Assets/Scripts/CubePiece.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CubePiece : MonoBehaviour
+{
+    public enum WoodType
+    {
+        Yellow,
+        Blue,
+        Purple,
+        Red
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Wood", menuName = "Wood")]
+public class Wood : ScriptableObject
+{
+    public new string name;
+
+    public Sprite woodSprite;
+
+    public CubePiece.WoodType woodType;
+}
